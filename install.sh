094f9488c13735c48b65a300bd2109c28915a773
Vambo3 <101197323+Vambo-M@users.noreply.github.com>
2022-05-10 20:45:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ pkg install ffmpeg -y
 pkg install imagemagick -y
 npm install
 npm update
+http://api.brainshop.ai/get?bid=166277&key=hnWmTkHfhd7ALauK&uid=[uid]&msg=[msg]
+mongodb+srv://MVambo:MaiSan4ever@cluster0.1s5sl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
